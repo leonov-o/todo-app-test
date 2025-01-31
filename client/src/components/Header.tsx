11ed8ac936a550ede27e79e09a3565c511a566c2
leonov-o <oleksandr.leonov.work@gmail.com>
2025-01-31 17:01:56 +0200

    Enhance Header: Add user store, logout, and navigation.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,23 +1,34 @@
 import {Button} from "@/components/ui/button.tsx";
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {Link, Outlet} from "react-router";
+import {useUserStore} from "@/store";
+import {AddNewDialog} from "@/components/AddNewDialog.tsx";
+import React from "react";
 
-export const Header = () => {
+export const Header: React.FC = () => {
+    const {user, fetchLogout} = useUserStore();
+
     return (
-        <div className="p-4 h-16 flex justify-between items-center bg-gray-200 rounded-b-lg">
-            <div className="">TodoList</div>
-            <div className="flex justify-between items-center">
-                <Button>Add new</Button>
-                <DropdownMenu >
-                    <DropdownMenuTrigger className="ml-4" asChild>
-                        <Button variant="outline">Hi, User!</Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuItem>
-                            Logout
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+        <div>
+            <div className="p-4 h-16 flex justify-between items-center bg-gray-200 rounded-b-lg">
+                <Link to="/">
+                    <div className="">TodoList</div>
+                </Link>
+                <div className="flex justify-between items-center">
+                    <AddNewDialog/>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="ml-4" asChild>
+                            <Button variant="outline">Hi, {user.name}!</Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem onClick={fetchLogout}>
+                                Logout
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </div>
+            <Outlet/>
         </div>
     );
 };
