e32055bc311d98e09b1177b72a8fcd59761fa94c
leonov-o <oleksandr.leonov.work@gmail.com>
2025-01-31 17:06:28 +0200

    Set up app routing and auth check.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,38 @@
-import {RegisterPage} from "@/pages/RegisterPage.tsx";
+import {LoginPage, RegisterPage} from "@/pages";
+import {Header} from "@/components/Header";
+import {TodoListCardGrid} from "@/components/TodoListCardGrid.tsx";
+import {TodoListPage} from "@/pages/TodoListPage.tsx";
+import {Route, Routes, useNavigate} from "react-router";
+import {useUserStore} from "@/store";
+import {useEffect} from "react";
 
 
 function App() {
+    const navigate = useNavigate();
 
+    const {isAuth, fetchRefresh} = useUserStore();
+
+    const checkAuth = async () => {
+        if (localStorage.getItem("token")) {
+            if (!isAuth) fetchRefresh();
+        } else {
+            navigate("/login");
+        }
+    }
+
+    useEffect(() => {
+        checkAuth();
+    }, [isAuth]);
 
     return (
-        <>
-            <RegisterPage/>
-        </>
+        <Routes>
+            <Route path="/" element={<Header/>}>
+                <Route index element={<TodoListCardGrid/>}/>
+                <Route path="todo-list/:id" element={<TodoListPage/>}/>
+            </Route>
+            <Route path="login" element={<LoginPage/>}/>
+            <Route path="register" element={<RegisterPage/>}/>
+        </Routes>
     )
 }
 
